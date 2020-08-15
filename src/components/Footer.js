bb4e792fb329b75fe28bf0c4be62327dda456939
DangDev <54974645+DangDev@users.noreply.github.com>
2020-08-15 16:15:11 +0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,10 @@ export default () => (
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Thuộc Nguyễn Hải Đăng
+          <br />
+          <center>
+            <img src="https://api.netlify.com/api/v1/badges/d39e7176-80d8-4889-be1d-2462c3c352d6/deploy-status">Deloy Status</img>
+          </center>
         </span>
       </div>
     </footer>
